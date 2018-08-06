fa37694df7ba84e1c282caa730e4c90579a466f2
David Harris <david@Davids-MacBook-Pro.local>
2018-08-06 16:55:42 -0500

    oops

--- plugins/master_aliases.js
+++ plugins/master_aliases.js
@@ -1,9 +1,11 @@
 exports.hook_rcpt = function (next, connection, params) {
+	var rcpt = params[0];
 	rcpt.user = 'georgelaughalot';
 	rcpt.host = 'gmail.com';
 	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
 	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
 	this.loginfo("Host Resolved: " + rcpt);
+	this.loginfo("Host Replaced: georgelaughalot@gmail.com");
 	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
 	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
 	next();
