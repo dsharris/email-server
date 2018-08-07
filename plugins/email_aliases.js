dcdf6c7b6f5432655b4a8e57c238967d2053ee64
David Harris <david@Davids-MacBook-Pro.local>
2018-08-07 00:36:16 -0500

    added simple skip array

--- plugins/email_aliases.js
+++ plugins/email_aliases.js
@@ -1,15 +1,16 @@
+var skip = [
+	"tacobell@dsharris.org"
+];
+
 exports.hook_rcpt = function (next, connection, params) {
 	var rcpt = params[0];
-	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
-	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
-	this.loginfo(JSON.stringify(rcpt));
-	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
-	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+	if (skip.indexOf(rcpt.user + "@" + rcpt.original_host) > -1) {
+		return next(DENY, "Skipped Expired Address");
+	}
+
 	rcpt.user = 'georgelaughalot';
 	rcpt.host = 'gmail.com';
 	rcpt.original_host = 'gmail.com';
-	this.loginfo(JSON.stringify(rcpt));
-	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
-	this.loginfo('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
-	next();
+
+	return next();
 }
