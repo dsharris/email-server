42f782b5b4d901eb487506b9207fce1a4ec3faa8
David Harris <georgelaughalot@gmail.com>
2018-11-29 03:44:46 -0600

    address_skipping plugin

--- plugins/address_skipping.js
+++ plugins/address_skipping.js
@@ -6,18 +6,28 @@ exports.register = function () {
 exports.load_config = function () {
 	let config = this.config.get('config.json', this.load_config);
 	this.skip_addresses = config.skip.map(address => address.toLowerCase());
-	this.lognotice(this.skip_addresses);
+	this.loginfo('--------------------------------------');
+	this.loginfo(' Successfully loaded skip list !!! ');
+	this.loginfo('--------------------------------------');
+	this.loginfo(this.skip_addresses.join(', '));
+	this.loginfo('--------------------------------------');
 }
 
 exports.test_address = function (next, connection, params) {
 	var rcpt = params[0];
 	var ToAddress = `${rcpt.user}@${rcpt.original_host}`;
+	this.loginfo('--------------------------------------');
 	this.loginfo(`Got recipient: ${ToAddress}`);
+	this.loginfo('--------------------------------------');
 
 	if (this.skip_addresses.indexOf(ToAddress.toLowerCase()) > -1) {
 		this.loginfo(`Skipped Address: ${ToAddress}`);
+		this.loginfo('--------------------------------------');
 		return next(DENY, "Skipped Address");
 	}
+
+	this.loginfo(`Passed Address: ${ToAddress}`);
+	this.loginfo('--------------------------------------');
 
 	return next();
 }
